562b44c3cfa6c6ac98cf17e374ac911fa6699c2c
Your Name <you@example.com>
2021-02-12 23:10:47 +0100

    Add joke controller

---
+++ src/main/java/es/lareira/joke/jokeapp/controllers/JobController.java
@@ -0,0 +1,21 @@
+package es.lareira.joke.jokeapp.controllers;
+
+import es.lareira.joke.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+    private final JokeService jokeService;
+
+    public JobController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", this.jokeService.getJoke());
+        return "chucknorris";
+    }
+}
